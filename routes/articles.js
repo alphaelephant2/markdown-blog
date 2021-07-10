2e66961aa6983c2c3ae8ba0a13c7718e8fa2ed1d
Mohammad Rizki Pragustono <rizkipragustono@gmail.com>
2021-07-10 13:45:30 +0700

    fixed where it couldnt make new a article and edit an article

--- routes/articles.js
+++ routes/articles.js
@@ -17,12 +17,12 @@ router.get('/:slug', async (req, res) => {
     res.render('articles/show', { article: article })
 })
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
     req.article = new Article()
     next()
 }, saveArticleAndRedirect('new'))
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', async (req, res, next) => {
     req.article = await Article.findById(req.params.id)
     next()
 }, saveArticleAndRedirect('edit'))
